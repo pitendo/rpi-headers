3b44c55fac08e15241f49cbcfce63a6a7ef29b21
Christian <chrisis>
2014-03-01 15:00:15 +0100

    Made some minor adjustments

--- get-rpi-headers.sh
+++ get-rpi-headers.sh
@@ -5,10 +5,9 @@ KVERSION=`uname -r`
 
 cd /usr/src
 wget  https://github.com/raspberrypi/linux/tarball/rpi-$KTREE
-mv rpi-$KTREE
 tar xzf rpi-$KTREE
 
-mv raspberry-linux-* linux-headers-$KVERSION
+mv raspberrypi-linux-* linux-headers-$KVERSION
 cd linux-headers-$KVERSION
 zcat /proc/config.gz > .config
 make oldconfig
