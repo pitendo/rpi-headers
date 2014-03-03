9d37b92c25b6a9ae4267cda6936f269eb70998e5
Christian <chrisis>
2014-03-03 19:29:43 +0100

    Fixed error with linking to kernel source and added comments

--- get-rpi-headers.sh
+++ get-rpi-headers.sh
@@ -5,24 +5,25 @@ KVERSION=`uname -r`
 
 cd /usr/src
 
-echo "Downloading kernel source"
+echo "### Downloading kernel source ###"
 wget  https://github.com/raspberrypi/linux/tarball/rpi-$KTREE
 
-echo "Uncompressing kernel source"
+echo "### Uncompressing kernel source ###"
 tar xzf rpi-$KTREE
 
 mv raspberrypi-linux-* linux-headers-$KVERSION
 rm -fr rpi-$KTREE
 
-echo "Prepare linux headers"
+echo "### Preparing linux headers ###"
 cd linux-headers-$KVERSION
 zcat /proc/config.gz > .config
 make oldconfig
 make modules_prepare
 
+echo "### Downloading Module.symvers ###"
 wget https://github.com/raspberrypi/firmware/raw/master/extra/Module.symvers
 
+echo "### Linking to kernel source ###"
 KERNEL_SRC=`pwd`
-pushd /lib/modules/$KVERSION
+cd /lib/modules/$KVERSION
 ln -s ${KERNEL_SRC} build
-popd
