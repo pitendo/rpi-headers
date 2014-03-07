f70052c699461a43b56896d4595e3ccdfcda3ad9
Christian <chrisis>
2014-03-07 17:41:12 +0100

    Made major changes to the script.

--- get-rpi-headers.py
+++ get-rpi-headers.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import os, tempfile
 
-HeaderFilesList = 'header_files.list'
-KernelVersionFile = '.rpi_kernel_version'
+HeaderFilesList = os.path.realpath('header_files.list')
 GitHubBranch = 'rpi-'
 GitHubKernelSourceTarballURL = 'https://github.com/raspberrypi/linux/tarball/'
+KernelSourcePath = ''
+LinuxHeaderPath = ''
 
 # Execute linux terminal command and return response
 def executeCommandReturnResponse(cmd):
@@ -36,15 +37,15 @@ def joinPath(list):
 
 	return str
 
-# Recursive function that create the folder structure and download needed files from GitHub
-def collectFiles(fullPath, path):
+# Recursive function that create folder structure and copy kernel source file to new directory
+def copyFile(fullPath, path):
 	# Split file path
 	list = path.strip().split('/')
 
 	# If the length of list is 1, then list[0] is a file.
 	if len(list) == 1:
-		# Download file from GitHub.
-		os.system('wget https://github.com/raspberrypi/linux/raw/' + GitHubBranch + '/' + fullPath)
+		# Copy file to new folder
+		os.system('cp '  + KernelSourcePath + fullPath + ' .')
 	else:
 		# If the dir does not exist, create it.
 		if not os.path.exists(list[0]):
@@ -58,30 +59,59 @@ def collectFiles(fullPath, path):
 		collectFiles(fullPath, joinPath(list))
 
 
-def processFilesList(file):
-	# Open the file
+def copyAllFiles(file):
+	# Open the file.
 	f = open(file)
 	
-	# Read file line by line
+	# Read file line by line.
 	for line in f:
-		# Build folder structure and collect file listed one the line
+		# Go to base of Linux header folder.
+		os.chdir(LinuxHeaderPath)
+		# Build folder structure and collect file listed one the line.
 		collectFiles(line, line)
+
+
+#   __  __       _
+#  |  \/  |     (_)
+#  | \  / | __ _ _ _ __
+#  | |\/| |/ _` | | '_ \
+#  | |  | | (_| | | | | |
+#  |_|  |_|\__,_|_|_| |_|
+#
 
 # Get kernel version.
 kversion = executeCommandReturnResponse('uname -r')
+
+# Store path to Linux header folder.
+lhfolder = 'linux-headers-' + kversion
+
+# Check if Linux headers folder already exists.
+if os.path.exists(lhfolder):
+	# Prompt user that it already exists and exit.
+	print 'The folder "' + lhfolder + '" already exists!'
+	return 0
+
+# Create folder for Linux headers.
+os.mkdir(lhfolder)
+
+# Store current working directory.
+pwd = os.getcwd()
+
+# Save oath to Linux header folder.
+lhpath = pwd + '/' + lhfolder
 
 # Determine which GitHub branch to download from.
 numbers = kversion.split('.')
 GitHubBranch = GitHubBranch + numbers[0] + '.' + numbers[1] + '.y'
 
-
-# Download Linux kernel source
+# Download Linux kernel source.
 os.system('wget ' + GitHubKernelSourceTarballURL + GitHubBranch)
 
-# Unzip Linux kernel source
+# Unzip Linux kernel source.
 (fd, tempFolder) = tempfile.mkdtemp()
 os.system('tar -xzf ' + GitHubBranch + ' -C ' + tempFolder)
 
-# Create folder for Linux headers
+# Store path to base dir of kernel source.
+kspath = pwd + '/' + tempFolder
 
-processFilesList(HeaderFilesList)
+processFilesList(kspath)
